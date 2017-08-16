5793df7eca6a6fc11b46616a45cc7e969861d134
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-16 14:11:29 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace CalculaIdadeEmDias
     {
         static void Main(string[] args)
         {
+            int idadeAnos, idadeMeses, idadeDias, idadeDiasTotal;
+            Console.WriteLine("Digite a quantidade de anos, meses e dias da sua idade: ");
+            idadeAnos = Convert.ToInt32(Console.ReadLine());
+            idadeMeses = Convert.ToInt32(Console.ReadLine());
+            idadeDias = Convert.ToInt32(Console.ReadLine());
+            idadeDiasTotal = (idadeAnos * 365) + (idadeMeses * 30) + idadeDias; 
+            Console.Write("Sua idade em dias é: " + idadeDiasTotal + " dias.");
+            Console.ReadLine();
         }
     }
 }
